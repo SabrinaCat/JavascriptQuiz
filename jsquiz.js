000eeb6c96e3cc221fc8125675442c870c413ecc
SabrinaCat <Sabrina.glow22@gmail.com>
2020-03-21 17:11:50 -0500

    centered text of jumbotron and question Elems

---
+++ jsquiz.js
@@ -0,0 +1,14 @@
+var quizStart = document.getElementById('#startButton');
+var jumbotron = document.getElementById('#frontPage')
+//var quizArray 
+
+
+   // function myfunction () {
+   // document.getElementById("frontpage").style.display = "none";
+quizStart.addEventListener('click', function() {
+    if (jumbotron.style.display = 'block') {
+        jumbotron.style.display = 'none';
+        }
+    })
+    
+    
