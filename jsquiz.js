fd24aac1a3985ef58b6c06392bb2ac537c8bb6c3
SabrinaCat <Sabrina.glow22@gmail.com>
2020-03-22 00:04:24 -0500

    got my variables linked to my html properly

--- jsquiz.js
+++ jsquiz.js
@@ -1,7 +1,36 @@
 var quizStart = document.querySelector('#startQuiz');
 var jumbotron = document.querySelector('#frontPage');
-var questionBox = document.querySelector ('#questionBox');
-//var quizArray 
+var questionBox = document.querySelector('#questionBox');
+var answerInputs = document.querySelector('form-check-label');
+var questionInputs = document.getElementById ('qSection');
+var continueButton = document.getElementById('nextQ');
+var questionKey = [ ]
+var answerKey = [a1== true];[a3== true]
+
+function submitAnswer() {
+    answerKey;
+    var i = 0, len = answerKey.length;
+    var checked = false;
+    var userAnswer =[]
+    for(i=0; i < answerKey.length; i++ ) {
+        if(answerKey[i].checked) {
+          checked = true;
+          userAnswer = answers[i].value;
+        }
+     } 
+     // if user click submit button without selecting any option, alert box will say "please select choice answer".
+     if(!checked) {
+       alert("please select choice answer");
+       return;
+     }
+     // Correct answer
+     if(userAnswer === "Scripting") {
+        alert("Answer is correct!");
+     }
+     // incorrect answer
+     else {
+        alert("Answer is wrong!");
+     }}
 
 
 //define function
@@ -14,5 +43,16 @@ quizStart.addEventListener('click', function() {
   if (startDisplay == true); {
       jumbotron.style.display = 'none';
       questionBox.style.display = 'block';
-  }
-})
+     }
+});
+
+function nextQuestion () {
+    for (questionKey=1; i> questionKey.length; i++)
+    return submitAnswer;
+}
+continueButton.addEventListener('click', function() {
+//loop through questions but it will start at index 1 because question 1 already has appeared
+if ('click') {
+    nextQuestion;
+}
+});
