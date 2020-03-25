54267a269b82d3f5197debdeb776eae3862fcd2c
SabrinaCat <Sabrina.glow22@gmail.com>
2020-03-24 22:27:25 -0500

    last ditch effort

--- jsquiz.js
+++ jsquiz.js
@@ -1,56 +1,63 @@
-//Used once to start quiz
+
+//var theGoods = questions.js.object
 const startButton = document.querySelector('#startQuiz');
-//used in function startQuiz to hide front page and display questionBox
 const jumbotron = document.querySelector('#frontPage');
 const questionBox = document.querySelector('#questionBox');
-//where we will loop our titles and choices (questions and answers)
-var answerInputs = document.querySelectorAll('form-check-label');
 var questionInputs = document.getElementById ('qSection');
-// will be used to switch the question and answer sets
-const continueButton = document.getElementById('nextQ');
-//timer variables
+var answerInputs = document.querySelectorAll('form-check-label');
+var continueButton = document.getElementById('nextQ');
+var start = true;
 var timerText = document.getElementById('timer');
 var secondsLeft = 120;
 var timerId ;
 
 
-
+ 
 function getQuestion() {
-     
-    // get the question/answer object from your questions array based on the current question index
-    //for ( i=0; i<= titleArray.length; i++){
-
-    // update the DOM with the current question
-   //questionInputs.textContent = titleArray [i];
-   // };
-    // update the DOM with the current answer choices
-    //answerInputs.textContent = theGoods[currentQ].choices;
-    // for loop that goes through the answer choices
-      //  for( j=0; j<=theGoods.choices.length; j++) {
-       // answerInputs.appendChild(theGoods.choices[j]);
-
-// };
     
-                // creates a button
-				// set a class attribute of "choice"
-				// set a value attribute of choice
-				// set text content of button to be choice
-                // append button to the DOM
-        
+    var i = 0;
+    // get the question/answer object from your questions array based on the current question index
+    for ( i=0; i< theGoods.length; i++) {
+        let j = i
+        for( j=0; j< theGoods[i].title.length; j++){
+    // update the DOM with the current question
+    
+    document.querySelector("#qSection").innerHTML= theGoods.title[j]
+    // update the DOM with the current answer choices
+    document.querySelectorAll("#form-check-label").innerHTML= theGoods.title[j].choices['']
+        };};
+    
+   
+ 
+      
+        // creates a button
+         var button = document.createElement("BUTTON");
 
-    }; 
+        // set a class attribute of "choice"
+         button.setAttribute("class", "choice");
+
+        // set a value attribute of choice
+         button.setAttribute( value = "Choice1");
+
+        // set text content of button to be choice
+        button.textContent = "Choice Options"
+        // append button to the DOM
+        
+        document.body.appendChild(button);
+    };
+   
  
 
 
 
 function startDisplay () {
     if (jumbotron.style.display = 'block', questionBox.style.display = 'none') {
-    return (startDisplay===true);
+    return (start===true);
 }};
 
-function startQuiz () {
+function countdown () {
     var computerSec = 1000;
-     timerId = setInterval(function(){
+        timerId = setInterval(function(){
         secondsLeft -= 1;
         timerText.textContent = secondsLeft + " seconds left!";
         console.log( secondsLeft);
@@ -64,14 +71,15 @@ function startQuiz () {
 };
 
 startButton.addEventListener('click', function() {
-    if (startDisplay=== true); {
+    if (start=== true); {
         jumbotron.style.display = 'none';
         questionBox.style.display = 'block';
-        startQuiz ();
+        countdown ();
         getQuestion ();
        }
-  });
+});
 
-  continueButton.addEventListener('click', function() {
-        getQuestion ();
-  });
+  continueButton.addEventListener('click', function () {
+    getQuestion ();
+    
+});
